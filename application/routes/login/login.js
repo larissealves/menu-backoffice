d7daede70c73ec8986ca66e9c18ef20c8a10c588
larisse.alves <alves.larisser@gmail.com>
2026-09-07 12:31:16 +0700

    fix: login function

--- application/routes/login/login.js
+++ application/routes/login/login.js
@@ -30,14 +30,6 @@ router.post('/login', async (req, res) => {
             }
         });
 
-
-        return res.status(200).json({
-            user: login.name,
-            loginIsValid: true,
-            message: 'Login realizado com sucesso.'
-        });
-
-
         const data = res.status(200).json({
             user: login.name,
             loginIsValid: login.logginValid,
